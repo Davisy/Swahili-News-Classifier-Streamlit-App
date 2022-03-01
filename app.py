07a6b990fd725980d4738cf1840dabf378186a1c
Davis David <davisdavid179@gmail.com>
2022-03-01 21:56:48 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import requests as r
+
+
+# add banner image
+st.header("Swahili News classifier App")
+st.image("images/swahili-news.png")
+st.subheader(
+    """
+A simple app to classify swahili news into different categories.
+"""
+)
+
+# form to collect news content
+my_form = st.form(key="news_form")
+news = my_form.text_input("Input your swahili news content here")
+submit = my_form.form_submit_button(label="Classify news content")
+
+
+if submit:
+    # classify news content by using the API URL provided
+
+    keys = {"news": news}
+    prediction = r.get(
+        "https://limitless-forest-00896.herokuapp.com/news-prediction/", params=keys
+    )
+
+    # collect and print the result
+    results = prediction.json()
+    category = results["prediction"]
+    probability = results["Probability"]
+
+    # Display results of the NLP task
+    st.header("Results")
+
+    st.write(
+        "News category is {} with a probabiliy of {} ".format(category, probability)
+    )
+
+
+st.write("Developed with Love by Davis David")
